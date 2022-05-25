482ec70f0675a8c3010b6a36638a28fe3f810f67
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-05-25 00:45:30 -0700

    Time: 7 ms (12.50%), Space: 2.3 MB (5.00%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.go
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.go
@@ -1,17 +1,14 @@
 func countStudents(students []int, sandwiches []int) int {
-	counter := 0
-	for len(students) != 0 {
+	misses := 0
+	for len(students) != misses {
 		if students[0] == sandwiches[0] {
 			students = students[1:]
 			sandwiches = sandwiches[1:]
-			counter = 0
+			misses = 0
 		} else {
 			students = append(students[1:], students[0])
-			counter++
-			if counter == len(students) {
-				return len(students)
-			}
+			misses++
 		}
 	}
-	return 0
+	return misses
 }
