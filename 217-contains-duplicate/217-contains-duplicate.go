5b2f50012270e5a433523e7a8dbcf24f8a307d09
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-05-25 05:38:43 -0700

    Time: 152 ms (12.65%), Space: 9.1 MB (42.83%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.go
+++ 217-contains-duplicate/217-contains-duplicate.go
@@ -1,8 +1,12 @@
 func containsDuplicate(nums []int) bool {
-    sort.Ints(nums) // Need O(N*log(N))
-    if len(nums)<2{return false}
-    for i:=1;i<len(nums);i++{
-        if nums[i]==nums[i-1] {return true}        
+    m := map[int]int{}   
+    
+    for _, val := range nums {
+        m[val]++
+        if m[val] > 1 {
+            return true
+        }
     }
+    
     return false
 }
