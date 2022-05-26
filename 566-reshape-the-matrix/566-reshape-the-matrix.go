3203cad5dc689a937879f86fc17576b3a95ddfec
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-05-26 01:10:07 -0700

    Time: 14 ms (47.08%), Space: 6.6 MB (21.54%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.go
+++ 566-reshape-the-matrix/566-reshape-the-matrix.go
@@ -14,11 +14,8 @@ func matrixReshape(mat [][]int, r int, c int) [][]int {
 	}
 
 	res := make([][]int, r)
-	for i := range res {
-		res[i] = make([]int, c)
-	}
-
 	for i := 0; i < r; i++ {
+        res[i] = make([]int, c)
 		for j := 0; j < c; j++ {
 			first := queue[0]
 			queue = queue[1:]
