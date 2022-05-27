91f8b4d2563d4b9732d7dc0d442459a7b313296a
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-05-27 05:24:36 -0700

    Time: 0 ms (100.00%), Space: 1.9 MB (78.57%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.go
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.go
@@ -6,21 +6,10 @@
  * }
  */
 func getDecimalValue(head *ListNode) int {
-    curr := head
-    
-    n := 0
-    for curr != nil {
-        curr = curr.Next
-        n++
+    ret := 0
+    for ; head != nil; head = head.Next {
+        fmt.Println(ret, ret << 1, head.Val, ret << 1 + head.Val)
+        ret = ret << 1 + head.Val
     }
-    
-    curr = head
-    sum := 0
-    
-    for curr != nil {
-        sum += curr.Val * int(math.Pow(2, float64(n-1)))
-        n--
-        curr = curr.Next
-    }
-    return sum
+    return ret
 }
