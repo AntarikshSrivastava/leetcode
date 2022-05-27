f24cb3061079232cc77aadeed1f0ad4b09e7763f
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-05-27 02:59:28 -0700

    Time: 3 ms (58.95%), Space: 2.5 MB (100.00%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.go
+++ 206-reverse-linked-list/206-reverse-linked-list.go
@@ -7,15 +7,13 @@
  */
 func reverseList(head *ListNode) *ListNode  {
 	var prev *ListNode
-	var curr *ListNode = head
-	for curr != nil {
-        fmt.Println("xxxx: ", curr)
+    curr := head
+    
+    for curr != nil {
         next := curr.Next
-		curr.Next = prev
-		prev = curr
-		curr = next 
-        
-        fmt.Println("head: ", head, "curr: ", curr, "prev: ", prev, "next: ", next) 
-	}
-	return prev
+        curr.Next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
 }
