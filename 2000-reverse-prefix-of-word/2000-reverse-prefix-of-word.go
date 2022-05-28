1d23f841ca909568bb59ac68faecd5b8aa76e229
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-05-28 05:45:41 -0700

    Time: 0 ms (100.00%), Space: 2.2 MB (21.88%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.go
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.go
@@ -1,24 +1,13 @@
 func reversePrefix(word string, ch byte) string {
+    s := ""
     
-    idx := -1
-    for i:=0; i<len(word); i++ {
-        if word[i] == ch {
-            idx = i
-            break
-        }   
+    for i, letter := range word {
+        s = string(letter) + s
+        
+        if letter == rune(ch) {
+            return s + word[i + 1 :]
+        }
     }
     
-    if idx == -1 {
-        return word
-    }
-    
-    return reverse(word[0:idx+1]) + word[idx+1:]
-}
-
-func reverse(s string) string {
-    r := []rune(s)
-    for i,j:=0,len(s)-1; i<j; i,j=i+1,j-1 {
-        r[i], r[j] = r[j], r[i]
-    }
-    return string(r)
+    return word
 }
