ebf4405d8066927367c27b718de9d3d14eb572c8
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-05-28 05:50:40 -0700

    Time: 22 ms (39.02%), Space: 7.4 MB (20.73%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.go
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.go
@@ -1,14 +1,13 @@
 func firstPalindrome(words []string) string {
-    ans := ""
     for _, word := range words {
-        if isPalindromic(word) { ans = word; break }
+        if isPalindromic(word) { return word }
     }
-    return ans
+    return ""
 }
 
 func isPalindromic(s string) bool {
     for l, r := 0, len(s) - 1; l < r; l, r = l + 1, r - 1 {
-        if s[l] != s[r] { return false; break }
+        if s[l] != s[r] { return false }
     }
     return true
 }
