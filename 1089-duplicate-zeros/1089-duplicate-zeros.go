f3c2d86091ce39a5969675f4f3cc2975659eb843
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-05-28 04:49:58 -0700

    Time: 10 ms (45.85%), Space: 3.3 MB (75.55%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.go
+++ 1089-duplicate-zeros/1089-duplicate-zeros.go
@@ -1,22 +1,12 @@
-func duplicateZeros(arr []int)  {
-    
-    stack := make([]int, 0)
-    i := 0
-    for len(stack) != len(arr) {
-        if arr[i] != 0 {
-            stack = append(stack, arr[i])
-        } else {
-            stack = append(stack, 0)
-            stack = append(stack, 0)
-            
-            if len(stack) > len(arr) {
-                stack = stack[:len(arr)]
-            }
-        }
-        i++
-    }
-    
-    for i:=0; i<len(arr); i++ {
-        arr[i] = stack[i]
-    }
+func duplicateZeros(arr []int) []int {
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		if arr[i] == 0 {
+			for j := length - 1; j > i; j-- {
+				arr[j] = arr[j-1]
+			}
+			i++
+		}
+	}
+	return arr
 }
