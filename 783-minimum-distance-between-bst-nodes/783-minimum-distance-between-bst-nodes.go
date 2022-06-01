441ec77af208031b0c820f465ad78727a46bd3e2
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-06-01 01:02:59 -0700

    Time: 0 ms (100.00%), Space: 2.4 MB (18.18%) - LeetHub

---
+++ 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDiffInBST(root *TreeNode) int {
+    nums := make([]int, 0)
+    inorder(root, &nums)
+    min := nums[1] - nums[0]
+    
+    for i:=2; i<len(nums); i++ {
+        if nums[i] - nums[i-1] < min {
+            min = nums[i] - nums[i-1]
+        }
+    }
+    
+    return min
+}
+
+func inorder(root *TreeNode, nums *[]int) {
+    if root == nil {
+        return
+    }
+    
+    inorder(root.Left, nums)
+    *nums = append(*nums, root.Val)
+    inorder(root.Right, nums)
+}
