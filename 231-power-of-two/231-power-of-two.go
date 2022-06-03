4516645c0c94c1764ec28a0bc955de3e8296b897
Antariksh Srivastava <antariksh.srivastava@gmail.com>
2022-06-02 21:27:37 -0700

    Time: 7 ms (15.34%), Space: 2 MB (100.00%) - LeetHub

--- 231-power-of-two/231-power-of-two.go
+++ 231-power-of-two/231-power-of-two.go
@@ -1,17 +1,9 @@
 func isPowerOfTwo(n int) bool {
-    if n <= 0{
+    if n == 0{
         return false
     }
-	if n == 1 || n== 2{
-		return true
-	}
-	for n > 2 {
-		if n%2 == 0 {
-			n /= 2
-		} else {
-			return false
-		}
-	}
-	return true
-
+    if n == 1 {
+        return true
+    }
+    return n%2==0 && isPowerOfTwo(n/2)
 }
